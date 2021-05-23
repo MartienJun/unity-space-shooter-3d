a5e43ab54e4ee1aed99bdc5a88d9f09a97777f15
MartienJun <martienjunaedi123@gmail.com>
2021-05-23 22:21:43 +0700

    feat: Add movement and boundary border script

---
+++ Assets/MyAssets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody RB;
+    public Boundary Border;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 Movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+        RB.velocity = Movement * Speed;
+
+        RB.position = new Vector3(
+            Mathf.Clamp(RB.position.x, Border.xMin, Border.xMax),
+            0.0f,
+            Mathf.Clamp(RB.position.z, Border.zMin, Border.zMax)
+        );
+    }
+}
