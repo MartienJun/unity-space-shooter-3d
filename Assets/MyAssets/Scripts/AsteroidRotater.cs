2ed58041cc6075c32db556c011c2d3c6f245425c
MartienJun <martienjunaedi123@gmail.com>
2021-05-23 23:12:35 +0700

    feat: Add script to rotate asteroit

---
+++ Assets/MyAssets/Scripts/AsteroidRotater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidRotater : MonoBehaviour
+{
+    public float Rotate;
+    public Rigidbody RB;
+
+    void Start()
+    {
+        RB.angularVelocity = Random.insideUnitSphere * Rotate;
+    }
+}
