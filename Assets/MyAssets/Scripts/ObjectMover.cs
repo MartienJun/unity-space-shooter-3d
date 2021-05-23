51e98f97c680b2809657a3b66b0c6d07d5fc35ba
MartienJun <martienjunaedi123@gmail.com>
2021-05-23 22:53:23 +0700

    feat: Create bullet and bullet scripts

---
+++ Assets/MyAssets/Scripts/ObjectMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody RB;
+    
+    void Start()
+    {
+        RB.velocity = transform.forward * Speed;
+    }
+}
