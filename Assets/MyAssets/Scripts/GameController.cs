b2d0a9965c7c9db080a9df84ab0528e9871b8cb4
MartienJun <martienjunaedi123@gmail.com>
2021-05-24 00:00:17 +0700

    feat: Asteroit generator

---
+++ Assets/MyAssets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Asteroit;
+    public Vector3 SpawnValue;
+    public int AsteroitCount;
+    public float SpawnWait;
+    public float StartWait;
+    public float WaveWait;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWave());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnWave()
+    {
+        yield return new WaitForSeconds(StartWait);
+        while(true)
+        {
+            for(int asteroit = 0; asteroit < AsteroitCount; asteroit++)
+            {
+                Vector3 SpawnPosition = new Vector3(Random.Range(
+                    (-SpawnValue.x), SpawnValue.x), 
+                    SpawnValue.y, 
+                    SpawnValue.z
+                );
+                Quaternion SpawnRotation = Quaternion.identity;
+                
+                Instantiate(Asteroit, SpawnPosition, SpawnRotation);
+
+                yield return new WaitForSeconds(SpawnWait);
+            }
+
+            yield return new WaitForSeconds(WaveWait);
+        }
+    }
+}
